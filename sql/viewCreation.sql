00a95b6c424fe0c85833df0cdcecf75c8ee6aaea
Thomas <thomaschar@hotmail.com>
2019-03-30 11:55:15 -0400

    updated num_hotel_area view to check current day availability

--- sql/viewCreation.sql
+++ sql/viewCreation.sql
@@ -1,9 +1,20 @@
 
 --We assume an area is a state
 CREATE VIEW ehotel.num_hotel_area AS 
-SELECT COUNT(rid) AS room_count, h.h_state
-FROM ehotel.room AS r, ehotel.hotel AS h
-WHERE r.hid = h.hid
+SELECT COUNT(r.rid) AS room_count, h.h_state
+FROM ehotel.hotel h
+JOIN ehotel.room r
+ON r.hid = h.hid
+WHERE r.rid NOT IN (
+	SELECT r.rid
+	FROM ehotel.room r
+	LEFT JOIN ehotel.booking b
+	ON r.rid = b.rid
+	LEFT JOIN ehotel.renting ren
+	ON r.rid = ren.rid
+	WHERE NOW() > b.start_date AND NOW() < b.end_date
+	OR NOW() > ren.start_date AND NOW() < ren.end_date
+)
 GROUP BY h.h_state;
 
 
