777076e69c5968f8381e51e0a6a5c7492f5b3621
Thomas <thomaschar@hotmail.com>
2019-04-04 14:53:53 -0400

    landing page

--- eHotels/Controllers/HomeController.cs
+++ eHotels/Controllers/HomeController.cs
@@ -41,27 +41,9 @@ namespace eHotels.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        public IActionResult HomePage()
-        {
-            return View();
         }
 
         public IActionResult EmployeeSection()
