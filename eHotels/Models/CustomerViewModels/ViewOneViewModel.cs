14649403a0c65e52c78aa0aebff256d889eb89a2
Vincent Harvey <vharv008@uottawa.ca>
2019-04-04 14:35:24 -0400

    Views controllers, pages and models

---
+++ eHotels/Models/CustomerViewModels/ViewOneViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eHotels.Models
+{
+    public class ViewOneViewModel
+    {
+        public List<ViewOne> viewones { get; set; }
+
+        public List<string> states { get; set; }
+
+        [Display(Name = "Select a state to display the numbers of rooms for this state.")]
+        public string stateSelected { get; set; }
+    }
+}
